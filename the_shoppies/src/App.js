4a679c16f2bbf31a72d60d84eb4e494b216e1489
Saakshi Shah <saakshishah@saakshis-mbp.lan>
2021-01-15 22:44:22 -0500

    Functionality done, tailwind installed

--- the_shoppies/src/App.js
+++ the_shoppies/src/App.js
@@ -1,64 +1,106 @@
-import React, { useState } from 'react';
-import { useFetch } from './hooks';
-import './App.css';
+import React, { useState } from "react";
+import { useFetch } from "./hooks";
+import "./App.css";
 
 const App = () => {
-	const [query, setQuery] = useState('');
+  const initialList = [];
+  const buttonsList = {};
+  const [query, setQuery] = useState("");
+  const [list, setList] = useState(initialList);
+  const [button, setButton] = useState(buttonsList);
 
   const url = query && `http://www.omdbapi.com/?s=${query}&apikey=3da302e6`;
 
+  const { status, data, error } = useFetch(url);
 
-	const { status, data, error } = useFetch(url);
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+    const query = e.target.search.value;
+    if (query) {
+      setQuery(query);
+      e.target.search.value = "";
+    }
+  };
 
-		const query = e.target.search.value;
-		if (query) {
-			setQuery(query);
-			e.target.search.value = '';
-		}
-	};
+  const movies = data.Search;
 
-	const movies = data.Search;
+  const addNomination = (imdbID, name, index) => {
+    const newList = list.concat({ name, imdbID });
+    setList(newList);
+    button[imdbID] = true;
+  };
 
-	return (
-		<div className="App">
-			<header> OMDB Search </header>
-			<form className="Form" onSubmit={handleSubmit}>
-				<input
-					type="text"
-					autoFocus
-					autoComplete="off"
-					name="search"
-					placeholder="Movie Title"
-				/>
-				<button> Search </button>
-			</form>
-			<main>
-				{status === 'idle' && (
-					<div> Let's get started by searching for a movie! </div>
-				)}
-				{status === 'error' && <div>{error}</div>}
-				{status === 'fetching' && <div className="loading"></div>}
-				{status === 'fetched' && (
-					<>
-						<div className="query"> Search results for {query} </div>
-						{movies.length === 0 && <div> No articles found! :( </div>}
-						{movies.map((movie) => (
-							<div className="movie" key={movie.imdbID}>
-								<a target="_blank" href={movie.Poster} rel="noopener noreferrer">
-									{movie.Title}
-								</a>{' '}
-								year: {movie.Year}
-                <button> Add </button>
-							</div>
-						))}
-					</>
-				)}
-			</main>
-		</div>
-	);
+  const removeNomination = (imdbID, name, index) => {
+    const newList = list.filter((item) => item.imdbID !== imdbID);
+    setList(newList);
+    button[imdbID] = false;
+  };
+
+  return (
+    <div className="App">
+      <header> OMDB Search </header>
+      <form className="Form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          autoFocus
+          autoComplete="off"
+          name="search"
+          placeholder="Movie Title"
+        />
+        <button> Search </button>
+      </form>
+      <main>
+        {status === "idle" && (
+          <div> Let's get started by searching for a movie! </div>
+        )}
+        {status === "error" && <div>{error}</div>}
+        {status === "fetching" && <div className="loading"></div>}
+        {status === "fetched" && (
+          <>
+            <div className="query"> Search results for {query} </div>
+            {movies.length === 0 && <div> No articles found! :( </div>}
+            {movies.map((movie, index) => (
+              <div className="movie" key={index}>
+                <a
+                  target="_blank"
+                  href={movie.Poster}
+                  rel="noopener noreferrer"
+                >
+                  {movie.Title}
+                </a>{" "}
+                year: {movie.Year}
+                {console.log(list.length)}
+                <button
+                  class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+                  onClick={() => {
+                    list.length <= 4
+                      ? addNomination(movie.imdbID, movie.Title)
+                      : alert("5 Nominations Reached");
+                  }}
+                  disabled={button[movie.imdbID]}
+                >
+                  Add{" "}
+                </button>
+              </div>
+            ))}
+          </>
+        )}
+      </main>
+      <div>
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>
+              {item.name}
+              <button onClick={() => removeNomination(item.imdbID, item.Title)}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default App;
