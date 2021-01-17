0e3923cd721ee7f237b30ff783f8df50f11f81f9
Saakshi Shah <saakshishah@saakshis-mbp.lan>
2021-01-16 21:10:36 -0500

    Significant interface changes made

--- the_shoppies/src/App.js
+++ the_shoppies/src/App.js
@@ -38,68 +38,153 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <header> OMDB Search </header>
-      <form className="Form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          autoFocus
-          autoComplete="off"
-          name="search"
-          placeholder="Movie Title"
-        />
-        <button> Search </button>
-      </form>
-      <main>
-        {status === "idle" && (
-          <div> Let's get started by searching for a movie! </div>
-        )}
-        {status === "error" && <div>{error}</div>}
-        {status === "fetching" && <div className="loading"></div>}
-        {status === "fetched" && (
-          <>
-            <div className="query"> Search results for {query} </div>
-            {movies.length === 0 && <div> No articles found! :( </div>}
-            {movies.map((movie, index) => (
+    <div class="bg-gray-50 bg-cover h-screen">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <header>
+          <h1 class="text-4xl font-bold leading-tight text-green-500">
+            The Shoppies
+          </h1>
+        </header>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            class="border-2 border-gray-300 mt-10 bg-white h-10 px-5 pr-16 rounded-lg text-l focus:outline-none"
+            type="text"
+            autoFocus
+            autoComplete="off"
+            name="search"
+            placeholder="Enter Movie Title"
+          ></input>
+          <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 ml-5">
+            {" "}
+            Search{" "}
+          </button>
+        </form>
+        <div class="grid grid-cols-2 gap-4">
+          <div>
+            {status === "idle" && (
+              <div class="mt-5">
+                {" "}
+                Let's get started by searching for a movie!{" "}
+              </div>
+            )}
+            {status === "error" && <div>{error}</div>}
+            {status === "fetching" && (
+              <div class="rounded-full animate-spin w-5 h-5 border-2 border-black">
+                {" "}
+              </div>
+            )}
+            {status === "fetched" && (
+              <>
+                <div class="mt-5"> Search results for {query} </div>
+                {data.Response === "False" && (
+                  <div> No articles found! :( </div>
+                )}
+                {movies.map((movie, index) => (
+                  <div className="movie" key={index}>
+                    <a
+                      target="_blank"
+                      href={movie.Poster}
+                      rel="noopener noreferrer"
+                    >
+                      {movie.Title}
+                    </a>{" "}
+                    year: {movie.Year}
+                    {console.log(list.length)}
+                    <button
+                      class="disabled:opacity-50 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 active:bg-green-700"
+                      onClick={() => {
+                        list.length <= 4
+                          ? addNomination(movie.imdbID, movie.Title)
+                          : alert("5 Nominations Reached");
+                      }}
+                      disabled={button[movie.imdbID]}
+                    >
+                      Add{" "}
+                    </button>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+          <div>
+            {list.map((item, index) => (
               <div className="movie" key={index}>
-                <a
-                  target="_blank"
-                  href={movie.Poster}
-                  rel="noopener noreferrer"
-                >
-                  {movie.Title}
-                </a>{" "}
-                year: {movie.Year}
-                {console.log(list.length)}
+                {item.name}
                 <button
-                  class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
-                  onClick={() => {
-                    list.length <= 4
-                      ? addNomination(movie.imdbID, movie.Title)
-                      : alert("5 Nominations Reached");
-                  }}
-                  disabled={button[movie.imdbID]}
+                  onClick={() => removeNomination(item.imdbID, item.Title)}
                 >
-                  Add{" "}
+                  Remove
                 </button>
               </div>
             ))}
-          </>
-        )}
-      </main>
-      <div>
-        <ul>
-          {list.map((item, index) => (
-            <li key={index}>
-              {item.name}
-              <button onClick={() => removeNomination(item.imdbID, item.Title)}>
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
+          </div>
+        </div>
       </div>
     </div>
+
+    // <div className="App">
+    //   <header> OMDB Search </header>
+    //   <form className="Form" onSubmit={handleSubmit}>
+    //     <input
+    //       type="text"
+    //       autoFocus
+    //       autoComplete="off"
+    //       name="search"
+    //       placeholder="Movie Title"
+    //     />
+    //     <button> Search </button>
+    //   </form>
+    //   <main>
+    //     {status === "idle" && (
+    //       <div> Let's get started by searching for a movie! </div>
+    //     )}
+    //     {status === "error" && <div>{error}</div>}
+    //     {status === "fetching" && <div className="loading"></div>}
+    //     {status === "fetched" && (
+    //       <>
+    //         <div className="query"> Search results for {query} </div>
+    //         {movies.length === 0 && <div> No articles found! :( </div>}
+    //         {movies.map((movie, index) => (
+    //           <div className="movie" key={index}>
+    //             <a
+    //               target="_blank"
+    //               href={movie.Poster}
+    //               rel="noopener noreferrer"
+    //             >
+    //               {movie.Title}
+    //             </a>{" "}
+    //             year: {movie.Year}
+    //             {console.log(list.length)}
+    //             <button
+    //               class="disabled:opacity-50 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+    //               onClick={() => {
+    //                 list.length <= 4
+    //                   ? addNomination(movie.imdbID, movie.Title)
+    //                   : alert("5 Nominations Reached");
+    //               }}
+    //               disabled={button[movie.imdbID]}
+    //             >
+    //               Add{" "}
+    //             </button>
+    //           </div>
+    //         ))}
+    //       </>
+    //     )}
+    //   </main>
+    //   <div>
+    //     <ul>
+    //       {list.map((item, index) => (
+    //         <li key={index}>
+    //           {item.name}
+    //           <button onClick={() => removeNomination(item.imdbID, item.Title)}>
+    //             Remove
+    //           </button>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </div>
   );
 };
 
--- the_shoppies/tailwind.config.js
+++ the_shoppies/tailwind.config.js
@@ -2,11 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+       spin: {
+         '0%': { transform: 'rotate(0deg) scale(1,1)' },
+         '50%': { transform: 'rotate(180deg) scale(1,1)' },
+         '100%': { transform: 'rotate(3600deg) ' },
+       }
+      },
+      animation: {
+        spin: 'spin 1s ease-out 0s infinite',
+       }
+    }
   },
   variants: {
     extend: {
-      opacity: ['disabled'],
+     backgroundColor: ['active'],
+     opacity: ['disabled'],
     }
   },
   plugins: [],
